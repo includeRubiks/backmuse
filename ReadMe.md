6797776da1dbefa283f2a65fb7a0628a74a16cde
includeRubiks <159594845+includeRubiks@users.noreply.github.com>
2024-09-10 16:24:28 -0400

    Done!
    So I just copy and pasted my code from the og project, then now im done.

--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,8 @@
-# BackMuse
+# Gyatify
 ## Background music for all
 Have you ever been working, and thought you needed some music? I know I have.
 So like programmers do, I decided to make a background music app, and hence, this repo was born.
+Also the name is cause I'm gen-alpha.
 ## Noob's guide
 Clone this repo using the url above but add ".git" at the end. Put the mp3s in the mp3s folder and run main.py for
-BackMuse.
+Gyatify.
---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame, os
+
+pygame.init()
+screen = pygame.display.set_mode((720,720))
+clock = pygame.time.Clock()
+
+playbutton = pygame.image.load('icons/playbutton.png')
+pausebutton = pygame.image.load('icons/pausebutton.png')
+playing = False
+songfiles = os.listdir('mp3s')
+selection = 0
+sanserif = pygame.font.SysFont('Sans Serif',27)
+pygame.display.set_caption('Gyatify')
+pygame.mixer.music.set_endevent(pygame.USEREVENT+0)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			x,y = pygame.mouse.get_pos()
+			for i in range(len(songfiles)):
+				if x >= 20 and x <= 640 and y >= i*20+20 and y <= i*20+40:
+					selection = i
+			if x >= 660 and x <= 710 and y >= 20 and y <= 70:
+				if playing == False:
+					playing = True
+					pygame.mixer.music.stop()
+					pygame.mixer.music.load(f'mp3s/{songfiles[selection]}')
+					pygame.mixer.music.play()
+				elif playing == True:
+					playing = False
+					pygame.mixer.music.stop()
+		if event.type == pygame.USEREVENT+0:
+			playing = False
+	
+	screen.fill('black')
+	
+	pygame.draw.rect(screen,(200,200,255),pygame.Rect(0,selection*20+20,640,20))
+	
+	for i,j in enumerate(songfiles):
+		screen.blit(sanserif.render(j.replace('.mp3',''),False,'white'),(20,i*20+20))
+		
+	if playing == False:	
+		screen.blit(playbutton,(660,20))
+	else:
+		screen.blit(pausebutton,(660,20))
+	
+	pygame.draw.line(screen,'white',(640,0),(640,720))
+	
+	pygame.display.flip()
+	
+	clock.tick(60)
+	
+	
